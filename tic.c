c8c7152ea4c8c6d37aa01b38336f7ed28a97a9de
7415963987456321 <tetenhop@gmail.com>
2019-05-28 13:50:39 +0000

    Turn tabs to spaces because github is dumb

--- tic.c
+++ tic.c
@@ -5,7 +5,7 @@
 
 #define BOARDSIZE 3
 #define ARRAYBOUND BOARDSIZE-1
-#define SPACING "       "
+#define SPACING "        "
 void parsenum(int input);
 
 bool board[BOARDSIZE][BOARDSIZE];
@@ -14,135 +14,135 @@ bool turn = true;
 // TODO: Remove printf, replace with game functions
 
 void print_state() {
-	for(int i = 0; i < BOARDSIZE; i++) {
-		printf(SPACING);
-		for(int j = 0; j < BOARDSIZE; j++) {
-			occupied[i][j] == false 
-				? printf("_ ") 
-				: printf("%d ", board[i][j]);
-		}
-		printf("\n");
-	}
+    for(int i = 0; i < BOARDSIZE; i++) {
+        printf(SPACING);
+        for(int j = 0; j < BOARDSIZE; j++) {
+            occupied[i][j] == false 
+                ? printf("_ ") 
+                : printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 bool full(){
-	for (int i = 0; i < BOARDSIZE; i++) {
-		for (int j = 0; j < BOARDSIZE; j++) {
-			if(!occupied[i][j]){ return false; }
-		}	
-	}
-	return true;
+    for (int i = 0; i < BOARDSIZE; i++) {
+        for (int j = 0; j < BOARDSIZE; j++) {
+            if(!occupied[i][j]){ return false; }
+        }    
+    }
+    return true;
 }
 
 void gameover(int player){
-	printf("Game over \n");
-	if(player == -1) {
-		printf("Stalemate!\n");
-	}else{
-		printf("Player %d wins! \n", player);
-	}
-	print_state();
-	exit(EXIT_SUCCESS);
+    printf("Game over \n");
+    if(player == -1) {
+        printf("Stalemate!\n");
+    }else{
+        printf("Player %d wins! \n", player);
+    }
+    print_state();
+    exit(EXIT_SUCCESS);
 }
 
 void checkwin(){
-	bool brdV, brdH;
-	bool brdLR = board[0][0];
-	bool brdRL = board[ARRAYBOUND][0];	
-	int RL = 0;
-	int LR = 0;
+    bool brdV, brdH;
+    bool brdLR = board[0][0];
+    bool brdRL = board[ARRAYBOUND][0];    
+    int RL = 0;
+    int LR = 0;
 
-	for(int i = 0; i < BOARDSIZE; i++) {
-		brdV = board[i][0];
-		brdH = board[0][i];
+    for(int i = 0; i < BOARDSIZE; i++) {
+        brdV = board[i][0];
+        brdH = board[0][i];
 
-		// Horizontal 
-		for(int h = 0; h < BOARDSIZE; h++) {
-			if(!occupied[i][h] || brdV != board[i][h]){
-				break;
-			}else{
-				brdV = board[i][h];
-			}
+        // Horizontal 
+        for(int h = 0; h < BOARDSIZE; h++) {
+            if(!occupied[i][h] || brdV != board[i][h]){
+                break;
+            }else{
+                brdV = board[i][h];
+            }
 
-			if(h == ARRAYBOUND){
-				printf("HORIZONTAL: %d\n", i);
-				gameover(turn);
-			}
-		}
-		// Vertical 
-		for(int v = 0; v < BOARDSIZE; v++) {
-			if(!occupied[v][i] || brdH != board[v][i]){
-				break;
-			}else{
-				brdH = board[v][i];
-			}
+            if(h == ARRAYBOUND){
+                printf("HORIZONTAL: %d\n", i);
+                gameover(turn);
+            }
+        }
+        // Vertical 
+        for(int v = 0; v < BOARDSIZE; v++) {
+            if(!occupied[v][i] || brdH != board[v][i]){
+                break;
+            }else{
+                brdH = board[v][i];
+            }
 
-			if(v == ARRAYBOUND){
-				printf("VERTICAL: %d\n", i);
-				gameover(turn);
-			}
-		}
+            if(v == ARRAYBOUND){
+                printf("VERTICAL: %d\n", i);
+                gameover(turn);
+            }
+        }
 
-		//Diagonal
-		if(occupied[i][i] && (brdLR == board[i][i])){
-			brdLR = board[i][i];
-			LR++;
-		}
+        //Diagonal
+        if(occupied[i][i] && (brdLR == board[i][i])){
+            brdLR = board[i][i];
+            LR++;
+        }
 
-		if(occupied[i][ARRAYBOUND-i] && (brdRL == board[i][ARRAYBOUND-i])){
-			brdRL = board[i][ARRAYBOUND-i];
-			RL++;
-		}
+        if(occupied[i][ARRAYBOUND-i] && (brdRL == board[i][ARRAYBOUND-i])){
+            brdRL = board[i][ARRAYBOUND-i];
+            RL++;
+        }
 
-		if(LR == BOARDSIZE || RL == BOARDSIZE){
-			printf("DIAGONAL \n");
-			gameover(turn);
-		}
-	}
+        if(LR == BOARDSIZE || RL == BOARDSIZE){
+            printf("DIAGONAL \n");
+            gameover(turn);
+        }
+    }
 }
 
 
 void apply(int x,int y) {
-	if(occupied[y][x]) return;
-	board[y][x] = turn;
-	occupied[y][x] = true;
-	checkwin();
-	if(full()) gameover(-1);
-	turn = !turn;
+    if(occupied[y][x]) return;
+    board[y][x] = turn;
+    occupied[y][x] = true;
+    checkwin();
+    if(full()) gameover(-1);
+    turn = !turn;
 }
 
 void readnum(){
-	char line[10];
-	int num = 0;
-	while(fgets(line, sizeof(line), stdin) != NULL){
-		if(sscanf(line, "%1d", &num) != 1){
-			printf("Invalid input, please try again! \n");
-			continue;
-		}
-		parsenum(num-1);
-		printf("Player %d 's turn:", turn);
-	}
+    char line[10];
+    int num = 0;
+    while(fgets(line, sizeof(line), stdin) != NULL){
+        if(sscanf(line, "%1d", &num) != 1){
+            printf("Invalid input, please try again! \n");
+            continue;
+        }
+        parsenum(num-1);
+        printf("Player %d 's turn:", turn);
+    }
 }
 
 void intro(){
-	printf("Tic Tac Toe - in binary  with boolean arrays.\n"
-		   "The game is to be played using the numpad\n"
-		   "which represents positions on the game board,\n"
-		   "    press enter after each move... \n"
-			);
+    printf("Tic Tac Toe - in binary  with boolean arrays.\n"
+           "The game is to be played using the numpad\n"
+           "which represents positions on the game board,\n"
+           "    press enter after each move... \n"
+            );
 }
 
 void parsenum(int input){
-	if(input < 0 || input > BOARDSIZE*BOARDSIZE) return;
-	int y = ARRAYBOUND-(input/BOARDSIZE);
-	int x = (input%BOARDSIZE);
-	apply(x,y);
-	print_state();
+    if(input < 0 || input > BOARDSIZE*BOARDSIZE) return;
+    int y = ARRAYBOUND-(input/BOARDSIZE);
+    int x = (input%BOARDSIZE);
+    apply(x,y);
+    print_state();
 }
 
 int main(int argc, const char *argv[]) {
-	intro();
-	print_state();
-	readnum();
-	return 0;
+    intro();
+    print_state();
+    readnum();
+    return 0;
 }
